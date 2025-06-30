0c9e181f259f9f80732ad91494591b0d8fa6973a
DaddyJiAr <santiagojr0212@gmail.com>
2025-06-30 19:17:41 +0800

    added light/dark mode toggle, uses localstorage. sponsored by ai hahahha

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 const card = document.getElementById('inner-card');
+const toggle = document.getElementById('toggleSwitch');
 const links = document.querySelector('a');
+
+window.addEventListener('DOMContentLoaded', function () {
+  const isDark = localStorage.getItem('isDarkMode') === 'true';
+  if (isDark) {
+    document.body.classList.add('dark');
+    document.querySelector('#toggleSwitch').checked = true;
+  }
+});
 
 links.addEventListener('click', function (event){
   event.stopPropagation(); // para pag nagclick ka sa anchor tag, di mag transfer/ mapasa yung 'click' event sa parent
@@ -7,4 +16,17 @@ links.addEventListener('click', function (event){
 
 card.addEventListener('click', function (){
   card.classList.toggle('flip');
+});
+
+// localStorage.setItem('isDarkMode', false);
+toggle.addEventListener('click', function () {
+  const isDark = localStorage.getItem('isDarkMode') === 'true';
+
+  if (isDark) {
+    localStorage.setItem('isDarkMode', 'false');
+    document.body.classList.remove('dark');
+  } else {
+    localStorage.setItem('isDarkMode', 'true');
+    document.body.classList.add('dark');
+  }
 });
