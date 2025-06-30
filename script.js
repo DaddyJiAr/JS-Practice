39f14ef4f4054ae3b99f7682d10fcdace2cf86cb
DaddyJiAr <santiagojr0212@gmail.com>
2025-06-30 22:00:38 +0800

    lol added typing animation kineme

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const card = document.getElementById('inner-card');
 const toggle = document.getElementById('toggleSwitch');
 const links = document.querySelector('a');
+const h2 = document.getElementById('toType');
+const toType = document.getElementById('toType').textContent;
+
 
 window.addEventListener('DOMContentLoaded', function () {
   const isDark = localStorage.getItem('isDarkMode') === 'true';
@@ -8,6 +11,17 @@ window.addEventListener('DOMContentLoaded', function () {
     document.body.classList.add('dark');
     document.querySelector('#toggleSwitch').checked = true;
   }
+  
+  h2.innerHTML = "";
+  for(let i = 0; i < toType.length; i++){
+    setTimeout(() => {
+      h2.innerHTML += toType.charAt(i); 
+    }, i * 100); // need i-multiply since non-blocking si settimeout. so, parang calculate/schedule kung kelan 
+    // lalabas nangyayari dito. first letter = 0ms, second = 100ms, third = 200ms
+
+  }
+  
+
 });
 
 links.addEventListener('click', function (event){
